85e6055e94eed3d4523182bc3fedcc1ea796ed09
Shobhit21287 <102307364+Shobhit21287@users.noreply.github.com>
2022-11-24 18:44:05 +0530

    Update code1.c

--- Question1/part1/code1.c
+++ Question1/part1/code1.c
@@ -1,109 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <pthread.h>
 #include <time.h>
+#include <pthread.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 
-pthread_attr_t attr1,attr2,attr3;
-struct sched_param forA,forB,forC;
-
-void *countA(){
-	pthread_attr_setschedparam(&attr1,&forA);
-	struct timespec start,end;
-	clock_gettime(CLOCK_REALTIME,&start);
-	long int i = 1;
-	for(long int j = 1 ; j < pow(2,32) ; j++){
-		i++;
-	}
-	i = 0;
-	for(long int j = 1 ; j < pow(2,32) ; j++){
-		i++;
-	}
-	clock_gettime(CLOCK_REALTIME,&end);
-	double final =(double)((end.tv_sec - start.tv_sec)*1000000000 + (end.tv_nsec - start.tv_nsec))/1000000000;
-	printf("thread OTHER : %f\n",final);
-	return NULL;
+void * countA(){
+    struct timespec start,end;
+    clock_gettime(CLOCK_REALTIME,&start);
+    long int i = 0;
+    for(long int i = 0 ; j < pow(2,32) ; j++){
+        i++;
+    }
+    clock_gettime(CLOCK_REALTIME,&end);
+    double final = ((end.tv_sec - start.tv_sec)*1000000000 + (end.tv_nsec - start.tv_nsec))*1000000000 ;
+    printf("Thread A : %f\n",final);
 }
 
-void *countB(){
-	pthread_attr_setschedparam(&attr2,&forB);
-	struct timespec start,end;
-	clock_gettime(CLOCK_REALTIME,&start);
-	long int i = 1;
-	for(long int j = 1 ; j < pow(2,32) ; j++){
-		i++;
-	}
-	i = 0;
-	for(long int j = 1 ; j < pow(2,32) ; j++){
-		i++;
-	}
-	clock_gettime(CLOCK_REALTIME,&end);
-	double final =(double) ((end.tv_sec - start.tv_sec)*1000000000 + (end.tv_nsec - start.tv_nsec))/1000000000;
-	printf("Thread RR : %f\n",final);
-	return NULL;
+void * countB(){
+    struct timespec start,end;
+    clock_gettime(CLOCK_REALTIME,&start);
+    long int i = 0;
+    for(long int i = 0 ; j < pow(2,32) ; j++){
+        i++;
+    }
+    clock_gettime(CLOCK_REALTIME,&end);
+    double final = ((end.tv_sec - start.tv_sec)*1000000000 + (end.tv_nsec - start.tv_nsec))*1000000000 ;
+    printf("Thread B : %f\n",final);
 }
 
-void *countC(){
-	pthread_attr_setschedparam(&attr3,&forC);
-	struct timespec start,end ;
-	clock_gettime(CLOCK_REALTIME,&start);
-	long int i = 1;
-	for(long int j = 1 ; j < pow(2,32) ; j++){
-		i++;
-	}
-	i = 0;
-	for(long int j = 0 ; j < pow(2,32) ; j++){
-		i++;
-	}
-	clock_gettime(CLOCK_REALTIME,&end);
-	double final =(double)((end.tv_sec - start.tv_sec)*1000000000 + (end.tv_nsec - start.tv_nsec))/1000000000 ;
-	printf("Thread FIFO : %f\n",final);
-	return NULL;
+void * countC(){
+    struct timespec start,end;
+    clock_gettime(CLOCK_REALTIME,&start);
+    long int i = 0;
+    for(long int i = 0 ; j < pow(2,32) ; j++){
+        i++;
+    }
+    clock_gettime(CLOCK_REALTIME,&end);
+    double final = ((end.tv_sec - start.tv_sec)*1000000000 + (end.tv_nsec - start.tv_nsec))*1000000000 ;
+    printf("Thread C : %f\n",final);
 }
+int main(){
+    pthread thrA,thrB,thrC;
+    struct sched_param fora,forb,forc;
+    fora.sched_priority = 0;
+    forb.sched_priority = 0;
+    forc.sched_priority = 0;
 
-void main(){
-	pthread_t ThrA,ThrB,ThrC;
-	
-	struct sched_param forA,forB,forC;
+    pthread_setschedparam(thrA,SCHED_OTHER,fora);
+    pthread_setschedparam(thrB,SCHED_FIFO,forb);
+    pthread_setschedparam(thrC,SCHED_RR,forc);
 
-	forA.sched_priority = 0;
-	forB.sched_priority = 99;
-	forC.sched_priority = 99;
-
-	pthread_attr_init(&attr1);
-	pthread_attr_init(&attr2);
-	pthread_attr_init(&attr3);
-
-	pthread_attr_setinheritsched(&attr1,PTHREAD_EXPLICIT_SCHED);
-	pthread_attr_setinheritsched(&attr2,PTHREAD_EXPLICIT_SCHED);
-	pthread_attr_setinheritsched(&attr3,PTHREAD_EXPLICIT_SCHED);
-	
-	pthread_attr_setschedpolicy(&attr1,SCHED_OTHER);
-	pthread_attr_setschedpolicy(&attr2,SCHED_RR);
-	pthread_attr_setschedpolicy(&attr3,SCHED_FIFO);
-
-	pthread_attr_setschedparam(&attr1,&forA);
-	pthread_attr_setschedparam(&attr2,&forB);
-	pthread_attr_setschedparam(&attr3,&forC);
-
-	/*pthread_setschedparam(ThrA,1,&forA);
-	pthread_setschedparam(ThrB,2,&forB);
-	pthread_setschedparam(ThrC,4,&forC);*/
-
-	pthread_create(&ThrA,&attr1,countA,NULL);
-	pthread_create(&ThrB,&attr2,countB,NULL);
-	pthread_create(&ThrC,&attr3,countC,NULL);
-
-	pthread_join(ThrA,NULL);
-	pthread_attr_destroy(&attr1);
-	pthread_join(ThrB,NULL);
-	pthread_attr_destroy(&attr2);
-	pthread_join(ThrC,NULL);
-	pthread_attr_destroy(&attr3);
-	
-	return;
-
+    pthread_create(&thrA,NULL,countA,NULL);
+    pthread_create(&thrB,NULL,countB,NULL);
+    pthread_create(&thrC,NULL,countC,NULL);
 }
-
-
